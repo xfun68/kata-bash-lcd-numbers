48451d799ad75d9ba5d0fb972be15e1b0e671252
Xiaofeng Wang <xfun68@gmail.com>
2019-03-01 15:09:15 +0800

    add templates

--- lcd-convert.sh
+++ lcd-convert.sh
@@ -5,7 +5,7 @@ mkdir -p /tmp/lcd-numbers && rm -rf /tmp/lcd-numbers/*
 (( id = 1 ))
 
 for digit in `echo $1 | sed 's/./ &/g'`; do
-    cp /tmp/${digit}.lcd /tmp/lcd-numbers/`printf '%03d' $id`.lcd
+    cp `dirname "$0"`/templates/${digit}.lcd /tmp/lcd-numbers/`printf '%03d' $id`.lcd
     echo $id
     (( id++ ))
 done
